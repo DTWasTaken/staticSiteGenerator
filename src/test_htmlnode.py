160bd925726ab6df4252df535001f8f9dfebb5b9
Dan T <github@danielterry.org>
2026-05-18 13:30:34 -0400

    CH2 L2-3

---
+++ src/test_htmlnode.py
@@ -0,0 +1,24 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode(props={"href": "https://www.google.com","target": "_blank"})
+        props_to_html = node.props_to_html()
+        expected = ' href="https://www.google.com" target="_blank"'
+        self.assertEqual(props_to_html, expected)
+
+    def test_props_to_html_none(self):
+        node = HTMLNode()
+        props_to_html = node.props_to_html()
+        expected = ""
+        self.assertEqual(props_to_html, expected)
+
+    def test_props_to_html_empty(self):
+        node = HTMLNode(props={})
+        props_to_html = node.props_to_html()
+        expected = ""
+        self.assertEqual(props_to_html, expected)
+
+if __name__ == "__main__":
+    unittest.main()
