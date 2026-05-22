32c25c589c24418b11cabe7f8e66e8c8df3ac834
Dan T <github@danielterry.org>
2026-05-22 16:35:12 -0400

    CH4 L2

---
+++ src/block_to_block_type.py
@@ -0,0 +1,70 @@
+from enum import Enum
+import re
+
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code block"
+    QUOTE = "quote"
+    UNORDERED_LIST = "unordered list"
+    ORDERED_LIST = "ordered list"
+
+
+def block_to_block_type(md_block: str) -> BlockType:
+    match True:
+        case _ if re.match(r"^#{1,6} .*?$", md_block):
+            return BlockType.HEADING
+
+        case _ if _is_code_block(md_block):
+            return BlockType.CODE
+
+        case _ if _each_line_matches_regex(r"^> ?.*?$", md_block):
+            return BlockType.QUOTE
+
+        case _ if _each_line_matches_regex(r"^- .*?$", md_block):
+            return BlockType.UNORDERED_LIST
+
+        case _ if _is_ordered_list(md_block):
+            return BlockType.ORDERED_LIST
+
+        case _:
+            return BlockType.PARAGRAPH
+
+
+def _is_code_block(block: str) -> bool:
+    if not (
+        block.startswith("```\n")
+        and block.endswith("```")
+    ):
+        return False
+
+    return True
+
+
+def _each_line_matches_regex(expression: str, block: str) -> bool:
+    lines = block.split("\n")
+
+    if len(lines) == 0:
+        return False
+
+    for line in lines:
+       if not re.match(expression, line):
+           return False
+
+    return True
+
+
+def _is_ordered_list(block: str) -> bool:
+    lines = block.split("\n")
+
+    if len(lines) == 0:
+        return False
+
+    number = 1
+
+    for line in lines:
+        if not re.match(rf"^{str(number)}\. .*?$", line):
+            return False
+        number += 1
+
+    return True
---
+++ src/test_block_to_block_type.py
@@ -0,0 +1,174 @@
+import unittest
+from block_to_block_type import block_to_block_type, BlockType
+
+class TestBlockToBlockType(unittest.TestCase):
+    # md.strip() because we only expext strings that come from markdown_to_blocks()
+    def test_paragraph(self):
+        md = "Test"
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_paragraph_multiline(self):
+        md = """
+Test
+Line 2
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_h1(self):
+        md = "# Test"
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_h6(self):
+        md = "###### Test"
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.HEADING)
+
+    def test_h7(self):
+        md = "####### Test"
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_heading_multiline(self):
+        md = """
+# multiline
+not supported
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_heading_invalid(self):
+        md = "######test"
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_code(self):
+        md = """
+```
+this is some code
+```
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_code_multiline(self):
+        md = """
+```
+this is some code
+
+
+this is some more code
+```
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.CODE)
+
+    def test_code_invalid(self):
+        md = """
+```invalid
+this is some code
+
+
+this is some more code
+```
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_quote(self):
+        md = """
+> This is a quote
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_quote_multiline(self):
+        md = """
+> This is a quote
+>The space isn't required
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.QUOTE)
+
+    def test_quote_invalid(self):
+        md = """
+> This is a quote
+ >The space isn't required
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_unordered_list(self):
+        md = """
+- This is a list
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_unordered_list_multiline(self):
+        md = """
+- This is a list
+- item 2
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.UNORDERED_LIST)
+
+    def test_unordered_list_invalid(self):
+        md = """
+- This is a list
+-item 2
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_ordered_list(self):
+        md = """
+1. This is a list
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_ordered_list_multiline(self):
+        md = """
+1. This is a list
+2. item 2
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.ORDERED_LIST)
+
+    def test_ordered_list_no_space(self):
+        md = """
+1. This is a list
+2.item 2
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_ordered_list_no_period(self):
+        md = """
+1. This is a list
+2 item 2
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_ordered_list_non_sequential(self):
+        md = """
+1. This is a list
+3. item 2
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+    def test_ordered_list_not_start_at_1(self):
+        md = """
+2. This is a list
+3. item 2
+"""
+        block_type = block_to_block_type(md.strip())
+        self.assertEqual(block_type, BlockType.PARAGRAPH)
+
+if __name__ == "__main__":
+    unittest.main()
