1e54b00e3dce383a5d27cb8961f1d2d8b55e3f39
Dan T <github@danielterry.org>
2026-05-23 18:57:06 -0400

    CH4 L3

--- src/functions/markdown_to_html_node.py
+++ src/functions/markdown_to_html_node.py
@@ -1,26 +1,57 @@
+import re
+
 from src.types.html_node import HTMLNode
+from src.types.parent_node import ParentNode
+from src.types.leaf_node import LeafNode
 from src.functions.block_to_block_type import block_to_block_type
 from src.types.block_type import BlockType
+from src.functions.text_to_textnodes import text_to_textnodes
+from src.functions.textnode_to_htmlnode import textnode_to_htmlnode
+from src.types.text_node import TextNode
+from src.types.text_type import TextType
 
 
-def mardown_to_html_node(markdown: str) -> HTMLNode:
+def markdown_to_html_node(markdown: str) -> ParentNode:
     blocks = markdown_to_blocks(markdown)
+    block_nodes = []
 
     for block in blocks:
         block_type = block_to_block_type(block)
         match block_type:
             case BlockType.CODE:
-                pass
+                block_node = ParentNode("pre", [])
+                cleaned_text = _clean_code_md(block)
             case BlockType.HEADING:
-                pass
+                heading_depth = len(block) - len(block.lstrip("#"))
+                block_node = ParentNode(f"h{str(heading_depth)}", [])
+                cleaned_text = _clean_h_md(block, heading_depth)
             case BlockType.ORDERED_LIST:
-                pass
+                block_node = ParentNode("ol", [])
+                cleaned_text = _clean_list_md(block)
             case BlockType.PARAGRAPH:
-                pass
+                block_node = ParentNode("p", [])
+                cleaned_text = _clean_p_md(block)
             case BlockType.QUOTE:
-                pass
+                block_node = ParentNode("blockquote", [])
+                cleaned_text = _clean_blockquote_md(block)
             case BlockType.UNORDERED_LIST:
-                pass
+                block_node = ParentNode("ul", [])
+                cleaned_text = _clean_list_md(block)
+
+        if block_type is BlockType.CODE:
+            text_node = TextNode(cleaned_text, TextType.CODE)
+            child = textnode_to_htmlnode(text_node)
+            block_node.children.append(child)
+            block_nodes.append(block_node)
+            continue
+
+        children = _text_to_children(cleaned_text)
+        block_node.children.extend(children)
+        block_nodes.append(block_node)
+
+    parent_node = ParentNode("div", block_nodes)
+
+    return parent_node
 
 def markdown_to_blocks(markdown: str) -> list[str]:
     return_list = []
@@ -32,5 +63,44 @@ def markdown_to_blocks(markdown: str) -> list[str]:
 
     return return_list
 
-def text_to_children(text: str) -> list[HTMLNode]:
-    pass
+def _text_to_children(text: str) -> list[HTMLNode]:
+    text_nodes = text_to_textnodes(text)
+    return_list = []
+    for text_node in text_nodes:
+        return_list.append(textnode_to_htmlnode(text_node))
+
+    return return_list
+
+def _clean_code_md(text: str) -> str:
+    return text[4:-3]
+
+def _clean_p_md(text: str) -> str:
+    return text.replace("\n", " ")
+
+def _clean_h_md(text: str, heading_depth: int) -> str:
+    return text[heading_depth + 1:]
+
+def _clean_list_md(text: str) -> str:
+    text_split = text.split("\n")
+    return_str = ""
+
+    for item in text_split:
+        _, rest_of_text = item.split(" ", 1)
+        return_str += "<li>" + rest_of_text + "</li>"
+
+    return return_str
+
+def _clean_blockquote_md(text: str) -> str:
+    text_split = text.split("\n")
+    temp_list = []
+
+    for item in text_split:
+        _, rest_of_text = item.split(">", 1)
+        if rest_of_text.startswith(" "):
+            _, rest_of_text = rest_of_text.split(" ", 1)
+        temp_list.append(rest_of_text)
+
+    return_str = "\n".join(temp_list)
+
+    return return_str
+
--- src/tests/test_func_markdown_to_html_node.py
+++ src/tests/test_func_markdown_to_html_node.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.functions.markdown_to_html_node import markdown_to_blocks
+from src.functions.markdown_to_html_node import markdown_to_blocks, markdown_to_html_node
 
 
 class TestMarkdownToBlocks(unittest.TestCase):
@@ -74,6 +74,92 @@ This is the same paragraph on a new line
                 "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line",
                 "- This is a list\n- with items",
             ],
+        )
+
+class TestMarkdownToBlocks(unittest.TestCase):
+    def test_paragraphs(self):
+        md = """
+This is **bolded** paragraph
+text in a p
+tag here
+
+This is another paragraph with _italic_ text and `code` here
+
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><p>This is <b>bolded</b> paragraph text in a p tag here</p><p>This is another paragraph with <i>italic</i> text and <code>code</code> here</p></div>",
+        )
+
+    def test_codeblock(self):
+        md = """
+```
+This is text that _should_ remain
+the **same** even with inline stuff
+```
+"""
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
+        )
+
+    def test_h1(self):
+        md = "# Test"
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(html, "<div><h1>Test</h1></div>")
+
+    def test_h6(self):
+        md = "###### Test with **bolded** word"
+
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><h6>Test with <b>bolded</b> word</h6></div>"
+        )
+
+    def test_ul(self):
+        md = """
+- This is a list
+- item 2
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ul><li>This is a list</li><li>item 2</li></ul></div>",
+        )
+
+    def test_ol(self):
+        md = """
+1. This is a list
+2. item 2
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><ol><li>This is a list</li><li>item 2</li></ol></div>"
+        )
+
+    def test_blockquote(self):
+        md = """
+> This is a quote
+>The space isn't required
+"""
+        node = markdown_to_html_node(md)
+        html = node.to_html()
+        self.assertEqual(
+            html,
+            "<div><blockquote>This is a quote\nThe space isn't required</blockquote></div>"
         )
 
 
